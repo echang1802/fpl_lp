b5adc84703ad874c4bf54e75e99f546848f942ec
Eloy Chang <=>
2022-03-04 18:03:36 -0300

    Added fpl api interface

---
+++ api_fpl/requests.py
@@ -0,0 +1,45 @@
+
+import yaml
+import requests
+
+class api:
+
+    def __init__(self):
+
+        self._read_conf()
+        self._get_base_info()
+
+
+    def _read_conf(self):
+        with open('confs.yml') as f:
+            confs = yaml.safe_load(f)["fpl"]
+            self._base_url = confs["base_url"]
+            self._endpoints = confs["endpoints"]
+
+
+    def _request(self, endpoint):
+        url = f"{self._base_url}{self._endpoints[endpoint]}"
+        result = requests.get(url)
+        return result.json()
+
+
+    def _get_base_info(self):
+        results = self._request("base")
+        self._parse_teams(results["teams"])
+        self._parse_players(results["elements"])
+
+
+    def _parse_teams(self, results):
+        self._teams = results # To do: Keep only values used
+
+
+    def _parse_players(self, results):
+        self._players = results # To do: Keep only values used
+
+
+    def total_players(self):
+        return len(self._players)
+
+
+    def players(self):
+        return self._players
